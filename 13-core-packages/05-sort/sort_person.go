d55c9211f44b2cc09eac8e19cd4dff79a3b3590f
mtraina <matteo.traina.mail@gmail.com>
2016-02-27 16:21:28 +0100

    sort

---
+++ 13-core-packages/05-sort/sort_person.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"sort"
+	"log"
+)
+
+type Person struct {
+	Name string
+	Age int
+}
+
+type ByName[] Person
+
+func (this ByName) Len() int {
+	return len(this)
+}
+
+func (this ByName) Less(i, j int) bool {
+	return this[i].Name < this[j].Name
+}
+
+func (this ByName) Swap(i, j int){
+	this[i], this[j] = this[j], this[i]
+}
+
+func main() {
+	kids := []Person{
+		{"Jill", 9},
+		{"Jack", 10},
+	}
+
+	sort.Sort(ByName(kids))
+	log.Println(kids)
+}
