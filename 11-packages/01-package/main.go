9d26648efa019f84b1fe8e8c48303528f5c2a6f7
mtraina <matteo.traina.mail@gmail.com>
2016-02-27 14:50:43 +0100

    package aliased

--- 11-packages/01-package/main.go
+++ 11-packages/01-package/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"introduction-to-go/11-packages/01-package/math"
+	m "introduction-to-go/11-packages/01-package/math"
 )
 
 func main() {
 	xs := []float64{1, 2, 3, 4}
-	avg := math.Average(xs)
+	avg := m.Average(xs)
 	fmt.Println(avg)
 }
